e6f332943648d46e5d01ea93c7209c613b754394
Joris Vink <joris@coders.se>
2018-04-18 19:57:56 +0000

    Define _GNU_SOURCE for linux platforms.

--- src/blog.c
+++ src/blog.c
@@ -14,6 +14,10 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#if defined(__linux__)
+#define _GNU_SOURCE
+#endif
+
 #include <sys/types.h>
 #include <sys/queue.h>
 #include <sys/stat.h>
